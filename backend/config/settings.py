dd8fa11e1140dd50864a865d45efae0545aae0fe
heitor-barbosa <heitor.barbosa@discente.ufg.br>
2026-04-26 16:44:58 -0300

    Criação do app de usuarios, e versao inicial do model de usuario

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -88,6 +90,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
